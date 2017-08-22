2b70051c80d1cef6e4a76bfaa694618c5b185379
ongaeshi <ongaeshi0621@gmail.com>
2017-08-22 23:20:50 +0900

    Convert Array to Color

--- OpenSiv3DRuby/Util.cpp
+++ OpenSiv3DRuby/Util.cpp
@@ -1,13 +1,33 @@
 #include "Util.hpp"
 
 #include "MrbColorF.hpp"
+#include "mruby/array.h"
 
 //----------------------------------------------------------
 namespace siv3druby {
 //----------------------------------------------------------
 Color Util::ToColor(mrb_state *mrb, mrb_value obj)
 {
-    if (mrb_obj_is_instance_of(mrb, obj, mrb_class_get(mrb, "ColorF"))) {
+    if (mrb_array_p(obj)) {
+        switch (mrb_ary_len(mrb, obj)) {
+            case 3:
+                return Color(
+                    mrb_int(mrb, mrb_ary_ref(mrb, obj, 0)),
+                    mrb_int(mrb, mrb_ary_ref(mrb, obj, 1)),
+                    mrb_int(mrb, mrb_ary_ref(mrb, obj, 2))
+                );
+            case 4:
+                return Color(
+                    mrb_int(mrb, mrb_ary_ref(mrb, obj, 0)),
+                    mrb_int(mrb, mrb_ary_ref(mrb, obj, 1)),
+                    mrb_int(mrb, mrb_ary_ref(mrb, obj, 2)),
+                    mrb_int(mrb, mrb_ary_ref(mrb, obj, 3))
+                    );
+            default:
+                mrb_raise(mrb, E_TYPE_ERROR, "Color Array size is 3 or 4");
+                return Color(0, 0, 0);
+        }
+    } else if (mrb_obj_is_instance_of(mrb, obj, mrb_class_get(mrb, "ColorF"))) {
         return MrbColorF::ToCpp(mrb, obj)->toColor();
     } else {
         mrb_raise(mrb, E_TYPE_ERROR, "wrong argument class");
--- OpenSiv3DRuby/main.rb
+++ OpenSiv3DRuby/main.rb
@@ -1,15 +1,15 @@
 # coding: utf-8
 class Palette
-  Black = ColorF.new(0, 0, 0)
+  Black = [0, 0, 0]
 end
 
-Graphics.set_background(ColorF.new(0.8, 0.9, 1.0))
+Graphics.set_background([204, 230, 255])
 
 font = Font.new(50)
 
 while System.update do
   c = Cursor.pos
-  Circle.new(c.x, c.y, 50).draw(ColorF.new(1, 0, 0, 0.5))
+  Circle.new(c.x, c.y, 50).draw([255, 0, 0, 128])
   font["Hello, Siv3D!🐣\n#{c}"].draw_at(Window.center, Palette::Black)
 end
 
