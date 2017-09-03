14b671d8a6067a2bd20eeda3d5ebf00ff843c04e
ongaeshi <ongaeshi0621@gmail.com>
2017-09-03 17:42:33 +0900

    Remove old comment

--- OpenSiv3DRuby/main.rb
+++ OpenSiv3DRuby/main.rb
@@ -1,8 +1,6 @@
-# coding: utf-8
 Graphics.set_background([204, 230, 255])
 
 font = Font.new(50)
-# cat = Texture.new("test.png")
 cat = Texture.new(Emoji.new("🐈"), TextureDesc::Mipped)
 
 while System.update do
@@ -12,16 +10,3 @@ while System.update do
   cat.resize(80, 80).draw(540, 380)
   cat.scale(2, 2).draw(0, 0)
 end
-
-# Goal
-# Graphics.set_background(ColorF.new(0.8, 0.9, 1.0))
-
-# font = Font.new(50)
-# cat = Texture.new(Emoji.new("🐈"), TextureDesc::Mipped)
-
-# while System.update do
-#   Circle.new(Cursor.pos, 60).draw(ColorF.new(1, 0, 0, 0.5))
-#   font["Hello, Siv3D!🐣"].draw_at(Window.center, Palette::Black)
-#   cat.resize(80).draw(540, 380)
-#   cat.scale(2, 2).draw(0, 0)
-# end
