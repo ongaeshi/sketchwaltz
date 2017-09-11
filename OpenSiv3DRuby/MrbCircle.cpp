c2b2b74a8d790421f3f6764021fb92cf561320c7
ongaeshi <ongaeshi0621@gmail.com>
2017-09-11 23:54:50 +0900

    MrbCircle use MrbObject

--- OpenSiv3DRuby/MrbCircle.cpp
+++ OpenSiv3DRuby/MrbCircle.cpp
@@ -1,42 +1,34 @@
 #include "MrbCircle.hpp"
 
-#include "MrbColorF.hpp"
-#include "MrbPoint.hpp"
 #include "Util.hpp"
-#include "mruby/array.h"
-#include "mruby/class.h"
-#include "mruby/data.h"
-#include "mruby/value.h"
 
 //----------------------------------------------------------
 namespace siv3druby {
-namespace {
-Circle& toCpp(mrb_value self)
+MrbObject<Circle>::Inner MrbCircle::fInner;
+
+//----------------------------------------------------------
+void MrbCircle::Init(mrb_state* mrb)
 {
-    return *((Circle*)DATA_PTR(self));
+    MrbObject::Init(mrb, "Circle");
+
+    mrb_define_method(mrb, Cc(), "initialize", initialize, MRB_ARGS_REQ(2));
+    mrb_define_method(mrb, Cc(), "draw", draw, MRB_ARGS_OPT(1));
 }
 
-void free(mrb_state *mrb, void *p)
-{
-    if (p) {
-        delete ((Circle*)p);
-    }
-}
-
-struct mrb_data_type data_type = { "siv3d_circle", free };
-
-mrb_value initialize(mrb_state *mrb, mrb_value self)
+//----------------------------------------------------------
+mrb_value MrbCircle::initialize(mrb_state *mrb, mrb_value self)
 {
     mrb_float x, y, r;
     mrb_get_args(mrb, "fff", &x, &y, &r);
 
     Circle* obj = new Circle(x, y, r);
 
-    mrb_data_init(self, obj, &data_type);
+    mrb_data_init(self, obj, DataType());
     return self;
 }
 
-mrb_value draw(mrb_state *mrb, mrb_value self)
+//----------------------------------------------------------
+mrb_value MrbCircle::draw(mrb_state *mrb, mrb_value self)
 {
     mrb_value color;
     int argc = mrb_get_args(mrb, "|o", &color);
@@ -44,54 +36,14 @@ mrb_value draw(mrb_state *mrb, mrb_value self)
     switch (argc)
     {
         case 1:
-            toCpp(self).draw(Util::ToColor(mrb, color));
+            ToCpp(self).draw(Util::ToColor(mrb, color));
             break;
         default:
-            toCpp(self).draw();
+            ToCpp(self).draw();
             break;
     }
 
     return mrb_nil_value();
-}
-
-}
-
-//----------------------------------------------------------
-void MrbCircle::Init(mrb_state* mrb)
-{
-    struct RClass *cc = mrb_define_class(mrb, "Circle", mrb->object_class);
-
-    mrb_define_method(mrb, cc, "initialize", initialize, MRB_ARGS_REQ(2));
-    mrb_define_method(mrb, cc, "draw", draw, MRB_ARGS_OPT(1));
-}
-
-//----------------------------------------------------------
-mrb_value MrbCircle::ToMrb(mrb_state *mrb, Circle *ptr)
-{
-    return ToMrb(mrb, mrb_class_get(mrb, "Circle"), ptr);
-}
-
-//----------------------------------------------------------
-mrb_value MrbCircle::ToMrb(mrb_state *mrb, struct RClass *cc, Circle *ptr)
-{
-    struct RData *data = mrb_data_object_alloc(mrb, cc, ptr, &data_type);
-    return mrb_obj_value(data);
-}
-
-//----------------------------------------------------------
-Circle* MrbCircle::ToCpp(mrb_state *mrb, mrb_value value)
-{
-    return ToCpp(mrb, mrb_class_get(mrb, "Circle"), value);
-}
-
-//----------------------------------------------------------
-Circle* MrbCircle::ToCpp(mrb_state *mrb, struct RClass *cc, mrb_value value)
-{
-    if (!mrb_obj_is_instance_of(mrb, value, cc)) {
-        mrb_raise(mrb, E_TYPE_ERROR, "wrong argument class");
-    }
-
-    return static_cast<Circle*>(DATA_PTR(value));
 }
 
 }
--- OpenSiv3DRuby/MrbCircle.hpp
+++ OpenSiv3DRuby/MrbCircle.hpp
@@ -1,16 +1,16 @@
 #pragma once
 
-#include <Siv3D.hpp>
-#include "mruby.h"
+#include "MrbObject.hpp"
 
 //----------------------------------------------------------
 namespace siv3druby {
-    class MrbCircle {
-    public:
-        static void Init(mrb_state *mrb);
-        static mrb_value ToMrb(mrb_state *mrb, Circle *ptr);
-        static mrb_value ToMrb(mrb_state *mrb, struct RClass *cc, Circle *ptr);
-        static Circle* ToCpp(mrb_state *mrb, mrb_value value);
-        static Circle* ToCpp(mrb_state *mrb, struct RClass *cc, mrb_value value);
-    };
+class MrbCircle
+    : public MrbObject<Circle>
+{
+public:
+    static void Init(mrb_state *mrb);
+private:
+    static mrb_value initialize(mrb_state *mrb, mrb_value self);
+    static mrb_value draw(mrb_state *mrb, mrb_value self);
+};
 }
