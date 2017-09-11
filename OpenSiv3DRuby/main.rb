bd8a2e520a3728e66b6b9f5dcc71181d542ba25e
ongaeshi <ongaeshi0621@gmail.com>
2017-09-12 01:32:58 +0900

    Add all class call

--- OpenSiv3DRuby/main.rb
+++ OpenSiv3DRuby/main.rb
@@ -3,11 +3,19 @@ Graphics.set_background([204, 230, 255])
 font = Font.new(50)
 cat = Texture.new(Emoji.new("🐈"), TextureDesc::Mipped)
 
+point = Point.new(1, 2)
+vec = Vec2.new(1.5, 2.1)
+p Point, point.x, point.y
+puts "#{Vec2}: (#{vec.x}, #{vec.y})"
+
 while System.update do
   c = Cursor.pos
+  Circle.new(600, 15, 15).draw(ColorF.new(1, 1, 1, 0.5))
+  Rect.new(90, 228, 40, 150).draw(Palette::Yellowgreen)
   Circle.new(c.x, c.y, 50).draw([255, 0, 0, 128])
+  Rect.new(150, 228, 40, 150).draw(Palette::Yellowgreen)
+  cat.scale(2, 2).draw(0, 0)
+  font["こんにちは"].draw(0, 0)
   font["Hello, Siv3D!🐣\n#{c}"].draw_at(Window.center.x, Window.center.y, Palette::Black)
   cat.resize(80, 80).draw(540, 380)
-  cat.scale(2, 2).draw(0, 0)
-  Rect.new(100, 200, 50, 150).draw(Palette::Black)
 end
