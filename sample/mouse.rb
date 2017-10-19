b1191b62ac57198446d20f2093c8041a7021b3d6
ongaeshi <ongaeshi0621@gmail.com>
2017-10-20 00:33:37 +0900

    Add sample/mouse.rb

---
+++ sample/mouse.rb
@@ -0,0 +1,58 @@
+$font = Font.new(40)
+
+def sample_case(title)
+  is_pause = false
+  rest = 60
+
+  while rest > 0 do
+    yield
+    $font[title].draw(0, 380)
+
+    if is_pause
+      Rect.new(580, 15, 12, 35).draw([255, 255, 255, 128])
+      Rect.new(600, 15, 12, 35).draw([255, 255, 255, 128])
+      Rect.new(565, 0, 15 + 20 + 12 + 15, 65).draw([255, 255, 255, 128])
+      is_pause ^= true if MouseL.down && Cursor.pos.x > 560 && Cursor.pos.y < 70
+    else
+      rest -= 1
+      is_pause ^= true if MouseL.down
+    end
+
+    Fiber.yield
+  end
+end
+
+fiber = Fiber.new do
+  loop do
+    sample_case("Cursor Pos") do
+      pos = Cursor.pos
+      Circle.new(pos.x, pos.y, 15).draw Palette::Yellow
+      $font[pos.to_s].draw
+    end
+
+    count = 0
+    sample_case("Mouse Down") do
+      count += 1 if MouseL.down
+      count -= 1 if MouseR.down
+      $font["#{count} times"].draw
+    end
+
+    count = 0
+    sample_case("Mouse Pressed") do
+      count += 1 if MouseL.pressed
+      count -= 1 if MouseR.pressed
+      $font["#{count} times"].draw
+    end
+
+    count = 0
+    sample_case("Mouse Up") do
+      count += 1 if MouseL.up
+      count -= 1 if MouseR.up
+      $font["#{count} times"].draw
+    end
+  end
+end
+
+while System.update do
+  fiber.resume
+end
