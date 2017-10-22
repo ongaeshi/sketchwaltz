23b90c2ae922618043f3a16cc6d9eef6a88b1273
ongaeshi <ongaeshi0621@gmail.com>
2017-10-22 22:56:26 +0900

    Add delta and wheel sample

--- sample/mouse.rb
+++ sample/mouse.rb
@@ -3,7 +3,7 @@ $font = Font.new(40)
 
 def sample_case(title)
   is_pause = false
-  rest = 60
+  rest = 90
 
   while rest > 0 do
     yield
@@ -51,6 +51,26 @@ fiber = Fiber.new do
       count -= 1 if MouseR.up
       $font["#{count} times"].draw
     end
+
+    pos = Point.new(200, 200)
+    sample_case("Delta Speed") do
+      delta = Cursor.delta
+      pos = Point.new(pos.x + delta.x, pos.y + delta.y)
+      # pos += delta
+      Circle.new(pos.x, pos.y, 15).draw(Palette::Yellow)
+      $font[delta.to_s].draw
+    end
+
+    pos = Point.new(200, 200)
+    sample_case("Wheel") do
+      y = Mouse.wheel
+      x = Mouse.wheelh
+      pos = Point.new(pos.x + x, pos.y + y)
+      # pos.x += x
+      # pos.y += y
+      Rect.new(pos.x, pos.y, 100, 100).draw(Palette::Yellow)
+    end
+
   end
 end
 
